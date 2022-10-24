b36f3f6444eb561a0a13427b0f9bd6bd2f3d52e1
Don <dagem.alemseged1@gmail.com>
2022-10-24 05:32:02 -0400

    Add button to let user create a new grid and decide no of pixels

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/*
+    Written by: Dagem Woldeyohannes
+    Date: October 24, 2022
+*/
+
+const cont = document.querySelector('.grid-container');
 const mainDiv = document.createElement('div');
 mainDiv.classList.add = 'main-container';
 
@@ -7,7 +13,6 @@ for (let i = 0; i < 16; i++) {
     row.classList.add('row');
     for (let j = 0; j < 16; j++) {
         const grid = document.createElement('div');
-        grid.focus();
         grid.classList.add('grid-style');
         grid.classList.add('grid' + (++counter));
         row.appendChild(grid);
@@ -18,9 +23,30 @@ for (let i = 0; i < 16; i++) {
     mainDiv.appendChild(row);
 }
 
-document.body.appendChild(mainDiv);
+cont.appendChild(mainDiv);
 
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', function() {
+    const noGrid = window.prompt('How many squares per side do you want on the new grid?');
+    cont.innerHTML = "";
 
-// for (let k = 0; k < (i * j); k++) {
-    
-// }
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add = 'main-container';
+
+    var counter = 0;
+    for (let i = 0; i < noGrid; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < noGrid; j++) {
+            const grid = document.createElement('div');
+            grid.classList.add('grid-style');
+            grid.classList.add('grid' + (++counter));
+            row.appendChild(grid);
+            grid.addEventListener('mouseover', function() {
+                grid.classList.add('grid-style-black');
+            });
+        }
+        mainDiv.appendChild(row);
+    }
+    cont.appendChild(mainDiv);
+});
